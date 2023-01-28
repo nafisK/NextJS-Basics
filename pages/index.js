b87f88d0ef82c5b0e537367db7a9d758a9cc8804
Nafis Khan <nafisrizwank@gmail.com>
2023-01-28 14:18:48 -0500

    creating layout component

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Navbar from '@/components/Navbar'
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Home Page</h1>
       <p>
         Consequat magna aliquip et sit veniam mollit aute nulla ipsum laborum
@@ -18,7 +17,6 @@ export default function Home() {
         Consequat magna aliquip et sit veniam mollit aute nulla ipsum laborum
         dolore magna.
       </p>
-      <Footer />
     </div>
   )
 }
