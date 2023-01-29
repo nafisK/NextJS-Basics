cdc75530806bde7aad95acae5c3f4c7dee7ec6da
Nafis Khan <nafisrizwank@gmail.com>
2023-01-29 12:58:26 -0500

    fetching data

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import Head from 'next/head'
+import styles from '@/styles/Ninjas.module.css'
 
-export default function index() {
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
+  return {
+    props: {
+      ninjas: data,
+    },
+  }
+}
+
+export default function index({ ninjas }) {
   return (
-    <div>
-      <h1>All Ninjas</h1>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Ninja Listing</title>
+        <meta name='keywords' content='ninjas' />
+      </Head>
+      <div>
+        <h1>All Ninjas</h1>
+        {ninjas.map(ninja => (
+          <div key={ninja.id}>
+            <a href={`/ninjas/${ninja.id}`} className={styles.single}>
+              <h3>{ninja.name}</h3>
+            </a>
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
